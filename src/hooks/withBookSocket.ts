e646fb742c72fc133dd9b651902d9efcfb39f8b3
justinkx <justinkizhakkeparampil@gmail.com>
2022-11-12 18:59:10 +0530

    send message after open

--- src/hooks/withBookSocket.ts
+++ src/hooks/withBookSocket.ts
@@ -4,29 +4,29 @@ import { connect } from '../socket';
 import { WithBookSocketOptions } from '../utils/types';
 import { getExchangeUrl } from '../helpers/exchangeUrls';
 
-export default function withBookSocket({ exchange }: WithBookSocketOptions) {
-  console.log({ exchange });
+export default function WithBookSocket({ exchange }: WithBookSocketOptions) {
   const url = getExchangeUrl(exchange);
 
   const onMessage = React.useCallback((e: any) => {
     const message = JSON.parse(e.data);
-    console.log(message);
   }, []);
 
   React.useEffect(() => {
     const ws: WebSocket = connect(url);
 
-    ws.send(
-      JSON.stringify({
-        channel: 'book',
-        event: 'subscribe',
-        freq: 'F0',
-        len: '100',
-        prec: 'P0',
-        subId: 'book/tBTCUSD/P0',
-        symbol: 'tBTCUSD',
-      })
-    );
+    ws.onopen = () => {
+      ws.send(
+        JSON.stringify({
+          channel: 'book',
+          event: 'subscribe',
+          freq: 'F0',
+          len: '100',
+          prec: 'P0',
+          subId: 'book/tBTCUSD/P0',
+          symbol: 'tBTCUSD',
+        })
+      );
+    };
 
     ws.onmessage = (e) => {
       onMessage(e);
